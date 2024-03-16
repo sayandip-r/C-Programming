3fab00aff0d0b2504a91fea510ec4ecb79e21228
sayandip-r <sayandiproy2005gmail.com>
2024-03-16 15:30:02 +0530

    my code

---
+++ basic c/1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    int a,b,c;
+    printf("Enter the first number:- ");
+    scanf("%d",&a);
+    printf(typeof(a))
+    printf("Enter the second number:- ")
+    scanf("")
+}
---
+++ codechef/course/LBCL02_problems_CLB007.c
@@ -0,0 +1,18 @@
+// Multiple Outputs
+// We can add as many printf() statements as we want in our code.
+
+// Task
+// Write a program which does the following
+
+// Add a printf statement and output the sum of 3 + 4
+// Add another printf statement and output the sum of 2 + 1.
+#include <stdio.h>
+
+void main() {
+	// add two printf statements here
+	int x=3,y=4;
+	int a=2,b=1;
+	printf("%d",x+y);
+	printf("%d",a+b);
+	
+}
