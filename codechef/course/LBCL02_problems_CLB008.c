cba2697e3e2de93100b7ff7f97f74478ba7ffa2e
sayandip-r <sayandiproy2005gmail.com>
2024-03-16 17:43:50 +0530

    my code

--- codechef/course/LBCL02_problems_CLB008.c
+++ codechef/course/LBCL02_problems_CLB008.c
@@ -18,6 +18,11 @@
 // // Output:
 // // 20
 // // 40
+// Task
+// Write a program which does the following
+
+// Output the sum of 3 and 4 using printf.
+// Output the sum of 1 and 2 using printf, but on a new line.
 #include <stdio.h>
 
 int main() {
@@ -27,3 +32,4 @@ int main() {
 	printf("%d\n", 1 + 2);
 	return 0;
 }
+
